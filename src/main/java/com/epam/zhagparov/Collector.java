f6936d10265ad77e3e83bda36f81df1fb52edc9e
Ali96kz <jakparov.kz@gmail.com>
2016-07-01 21:40:59 +0600

    add entity

--- src/main/java/com/epam/zhagparov/Collector.java
+++ src/main/java/com/epam/zhagparov/Collector.java
@@ -3,5 +3,5 @@ package com.epam.zhagparov;
 /**
  * Created by Арман on 01.07.2016.
  */
-public class HomeFlower {
+public class Collector {
 }
---
+++ src/main/java/com/epam/zhagparov/entity/Narcissus.java
@@ -0,0 +1,7 @@
+package com.epam.zhagparov.entity;
+
+/**
+ * Created by Арман on 01.07.2016.
+ */
+public class Narcissus {
+}
